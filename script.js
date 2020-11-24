af5e54eb9880e5ba5dce7e437fdd465fe97d0552
Sandra H <sanster@Sandras-MBP.lan>
2020-11-23 20:13:29 -0600

    setup semi workable search field.

--- script.js
+++ script.js
@@ -6,8 +6,34 @@ $( document ).ready(function() {
     // // $('#text-input').on('change', () => {
     // //   console.log('input was changed');
 
-    $('search').on('click', () => {
+
+
+
+
+    $('#search').on('click', () => {
       console.log('i was clicked');
+      console.log($('#text-input').val());
+      let searchTerm = $('#text-input').val();
+
+      console.log(searchTerm);
+
+      let url = "https://api.giphy.com/v1/gifs/search?api_key=3eG5OcLqnn9hoMlN0GSSgTw6CUwsYdre&q=" + searchTerm;
+
+      $.get(url, function (giphyResponse) {
+        console.log(giphyResponse);
+        console.log('giphyData', giphyResponse.data) 
+        console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
+        console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
+        console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+
+        for (let image of giphyResponse.data) {
+          let imgHTML = '<img src=${image.images.original.url} />'
+          $('#search-result').append(imgHtml);
+        }
+      });
+
+
+
     })
       
 });
@@ -33,4 +59,4 @@ $( document ).ready(function() {
 
 
 
-    //eI7GLewxQL9oxBoEuUURa8bMi1adzwgH//
+  
