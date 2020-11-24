11d4a26debccb40f141d6c7c9f4cc8e69feb26aa
Sandra H <sanster@Sandras-MBP.lan>
2020-11-23 19:34:39 -0600

    fixed for php error

--- script.js
+++ script.js
@@ -3,18 +3,14 @@
 $( document ).ready(function() {
      console.log( "ready!" );
 
-    $('#fetch-from-api').on('click', () => {
-      console.log('search was clicked');
-      
-      $.get('https://api.giphy.com/v1/gifs/search?api_key=eI7GLewxQL9oxBoEuUURa8bMi1adzwgH&q=corona', function(data) {
-        console.log(data);
-      
-      
-      });
+    // // $('#text-input').on('change', () => {
+    // //   console.log('input was changed');
 
+    $('search').on('click', () => {
+      console.log('i was clicked');
     })
-
-  });
+      
+});
 
 
 
