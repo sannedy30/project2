600ea057c9f9bb7543c2b5019e7b9a2426fb80d5
Sandra H <sanster@Sandras-MBP.lan>
2020-11-24 22:57:13 -0600

    made some changes to the how the search is input

--- script.js
+++ script.js
@@ -11,32 +11,50 @@ $( document ).ready(function() {
 
 
     $('#search').on('click', () => {
-      console.log('i was clicked');
-      console.log($('#text-input').val());
+      //  console.log('i was clicked');
+      //  console.log($('#text-input').val());
       let searchTerm = $('#text-input').val();
 
       console.log(searchTerm);
 
-      let url = "https://api.giphy.com/v1/gifs/search?api_key=3eG5OcLqnn9hoMlN0GSSgTw6CUwsYdre&q=" + searchTerm;
+      let url = 'https://api.giphy.com/v1/gifs/search?api_key=3eG5OcLqnn9hoMlN0GSSgTw6CUwsYdre&q=limit=5' + searchTerm;
 
       $.get(url, function (giphyResponse) {
         console.log(giphyResponse);
         console.log('giphyData', giphyResponse.data) 
         console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
         console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
+        console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original); 
+
         console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
 
+        let resultsHtml = '';
+
         for (let image of giphyResponse.data) {
-          let imgHtml = '<img src=${image.images.original.url} />'
-          $('#search-result').append(imgHtml);
+          console.log(image);
+          let imgHtml = '<img src=${image.images.original.url}/>'
+          resultsHtml = resultsHtml + imgHtml;
         }
+
+        console.log(resultsHtml);
+        $("#search-result").html(resultsHtml);
+
+        
+
+        
+
       });
 
-
-
     })
-      
+
 });
+
+
+         
+        
+
+
+  
 
 
 
